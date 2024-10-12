257b9eba1c6308135c2094281488494392eacff8
ikrammmmmmm <157960444+bebekgarut@users.noreply.github.com>
2024-10-12 10:46:42 +0700

    fe-0

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
 from flask import Flask, render_template, request
-from werkzeug.utils import secure_filename
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
-import os
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import classification_report, accuracy_score
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = './uploads'
-
-from sklearn.preprocessing import LabelEncoder
 
 # fungsi hitung knn
-def knn_classification(train_file, test_file, k):
-    # baco file
-    train_data = pd.read_csv(train_file)
-    test_data = pd.read_csv(test_file)
+def knn_classification(train_data, test_data, k):
     
     X_train = train_data.iloc[:, :-1]  # Atribut Training
     y_train = train_data.iloc[:, -1]   # Label Traning
@@ -30,45 +24,52 @@ def knn_classification(train_file, test_file, k):
             X_test[column] = le.transform(X_test[column])
             label_encoders[column] = le
 
-    # training
+    # training model
     knn = KNeighborsClassifier(n_neighbors=k)
     knn.fit(X_train, y_train)
 
     # Prediksi
     predictions = knn.predict(X_test)
 
+    accuracy = accuracy_score(y_test, predictions)
+    report = classification_report(y_test, predictions, output_dict=True)
+
     # Return prediksi dan label testing
-    return predictions, y_test.values
+    return predictions, y_test.values, accuracy, report
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     result = None
+    training_data = None
+    testing_data = None
+
     if request.method == 'POST':
       
         train_file = request.files['train_file']
         test_file = request.files['test_file']
         k_value = int(request.form['k_value'])
 
-        if train_file and test_file:
-            # Save ke uploads
-            train_filename = secure_filename(train_file.filename)
-            test_filename = secure_filename(test_file.filename)
-            train_file.save(os.path.join(app.config['UPLOAD_FOLDER'], train_filename))
-            test_file.save(os.path.join(app.config['UPLOAD_FOLDER'], test_filename))
+        try:
+            train_data = pd.read_csv(train_file)
+            test_data = pd.read_csv(test_file)
+        except Exception as e:
+            return f"Error reading files: {str(e)}", 400
 
-            # Perform KNN classification
-            train_path = os.path.join(app.config['UPLOAD_FOLDER'], train_filename)
-            test_path = os.path.join(app.config['UPLOAD_FOLDER'], test_filename)
+        predictions, actual_labels, accuracy, report = knn_classification(train_data, test_data, k_value)
 
-            predictions, actual_labels = knn_classification(train_path, test_path, k_value)
-
-            result = {
+        result = {
                 'predictions': predictions,
-                'actual_labels': actual_labels
-            }
+                'actual_labels': actual_labels,
+                'accuracy': accuracy,
+                'precision': report['weighted avg']['precision'],
+                'recall': report['weighted avg']['recall'],
+                'f1_score': report['weighted avg']['f1-score']
+        }
+        training_data = train_data
+        testing_data = test_data
 
-    return render_template('index.html', result=result, zip=zip)
+    return render_template('index.html', result=result, training_data=training_data, testing_data = testing_data, zip=zip)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
